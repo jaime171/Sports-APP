4c49c9e5be87dd6d65b1469787ad666e189025ef
jaime <jaimesalvador.simental@gmail.com>
2017-05-06 02:54:49 -0500

    gulp file update & watcher added

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,61 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
+const jshint = require('gulp-jshint');
 
-const _SOURCE_PATH = './';
+const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('styles', function(){
+const SOURCE_PATH = './';
 
-    this.src(_SOURCE_PATH + 'styles/main.sass')
+
+/* ====================
+ SET all tasks
+ ======================== */
+
+
+gulp.task('styles', styles);
+
+gulp.task('lint', scripts);
+
+gulp.task('watcher', watcher);
+
+gulp.task('default', ['styles', 'lint', 'watcher']);
+
+
+/* ====================
+ Task functions
+ ======================== */
+
+function watcher() {
+
+    gulp.watch(SOURCE_PATH + 'styles/*.{sass, scss}', {cwd: SOURCE_PATH}, ['styles']);
+
+    gulp.watch(SOURCE_PATH + 'scripts/main.js', {cwd: SOURCE_PATH}, ['lint']);
+
+    //gulp.watch('app/pug/*.pug', {cwd: src}, ['views']);
+
+}
+
+function scripts() {
+
+    return gulp.src(SOURCE_PATH + 'scripts/main.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(gulp.dest(SOURCE_PATH + 'public/scripts'));
+
+}
+
+function styles() {
+
+    this.src(SOURCE_PATH + 'styles/main.sass')
         .pipe(plumber())
         .pipe(sass())
-        .pipe(gulp.dest(_SOURCE_PATH + 'public/styles'));
-        //.pipe(browserSync.reload({stream: true}));
+        .pipe(gulp.dest(SOURCE_PATH + 'public/styles'));
 
-});
+}
 
-gulp.task('default', ['styles']);
+
+
+
